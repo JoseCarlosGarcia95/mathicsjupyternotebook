ae21fb9cb78c45a14ed5b2a0646baab60c0bc0a8
JoseCarlosGarcia95 <josecarlos.garciaortega@alum.uca.es>
2018-01-24 15:52:23 +0100

    Added static from mathics.

--- mathicsnotebook/kernel.py
+++ mathicsnotebook/kernel.py
@@ -18,12 +18,56 @@ class MathicsNotebookKernel(Kernel):
     }
 
     name = 'MathicsNotebook'
-    
+
+    """
+    Clear mathics output.
+    """
+    def clear_output(self, data):
+        data = re.sub(r"<math><mglyph width=\"(.*)\" height=\"(.*)\" src=\"(.*)\"/></math>", "<img width=\"\\1\" height=\"\\2\" src=\"\\3\" />", data, 0)
+        return data
+
+    def initialize_javascript_if_needed(self):
+        if self.execution_count == 1:
+            inject_javascript_code = """
+<script type="text/javascript">
+            function loadScript(url, callback){
+
+    var script = document.createElement("script")
+    script.type = "text/javascript";
+
+    if (script.readyState){  //IE
+        script.onreadystatechange = function(){
+            if (script.readyState == "loaded" ||
+                    script.readyState == "complete"){
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function(){
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
+</script>
+            """
+            
+            display_data = {
+                'data' : {'text/html' : inject_javascript_code},
+                'metadata' : {},
+            }
+            
+            self.send_response(self.iopub_socket, 'display_data', display_data)
     """
     Handle jupyter connections.
     """
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
+        self.initialize_javascript_if_needed()
+        
         if not silent:
             from mathics.core.parser import MultiLineFeeder
 
@@ -46,21 +90,22 @@ class MathicsNotebookKernel(Kernel):
                 raise
 
             for result in results:
-
-                html = result.get_data()['result']
-                html = re.sub(r"<math><mglyph width=\"(.*)\" height=\"(.*)\" src=\"(.*)\"/></math>", "<img width=\"\\1\" height=\"\\2\" src=\"\\3\" />", html, 0)
+                result_data = result.get_data()
+                
+                result_html = self.clear_output(result_data['result'])
                 
                 display_data = {
-                    'data' : {'text/html' : html},
+                    'data' : {'text/html' : result_html},
                     'metadata' : {},
                 }
        
                 self.send_response(self.iopub_socket, 'display_data', display_data)
 
-        return {'status': 'ok',
-                'execution_count': self.execution_count,
-                'payload': [],
-                'user_expressions': {},
+        return {
+            'status': 'ok',
+            'execution_count': self.execution_count,
+            'payload': [],
+            'user_expressions': {},
         }
 
 if __name__ == '__main__':
