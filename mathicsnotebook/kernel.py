e61e8d8b68510bdbfd711c8e52125f0fbe703577
JoseCarlosGarcia95 <josecarlos.garciaortega@alum.uca.es>
2018-02-02 15:37:32 +0100

    Added support for code completetion and clean up.

--- mathicsnotebook/kernel.py
+++ mathicsnotebook/kernel.py
@@ -20,57 +20,10 @@ class MathicsNotebookKernel(Kernel):
     name = 'MathicsNotebook'
 
     """
-    Clear mathics output.
-    """
-    def clear_output(self, data):
-        data = re.sub(r"<math><mglyph width=\"(.*)\" height=\"(.*)\" src=\"(.*)\"/></math>", "<img width=\"\\1\" height=\"\\2\" src=\"\\3\" />", data, 0)
-        return data
-
-    def read_static_file(self, static):
-        static_file = file('/home/jose/Proyectos/mathicsjupyternotebook/web/' + static, 'r')
-        content     = static_file.read()
-        static_file.close()
-        return content
-    def initialize_javascript_if_needed(self):
-        if self.execution_count == 1:
-            inject_javascript_code = '<script type="text/javascript">'
-
-            """
-<script type="text/javascript" src="/media/js/prototype/prototype.js"></script>
-<script type="text/javascript" src="/media/js/three/Three.js"></script>
-<script type="text/javascript" src="/media/js/three/Detector.js"></script>
-
-<script type="text/javascript" src="/media/js/mathjax/MathJax.js?config=MML_HTMLorMML&amp;delayStartupUntil=configured"></script>
-
-<script type="text/javascript" src="/media/js/message.js"></script>
-<script type="text/javascript" src="/media/js/authentication.js"></script>
-<script type="text/javascript" src="/media/js/inout.js"></script>
-<script type="text/javascript" src="/media/js/utils.js"></script>
-<script type="text/javascript" src="/media/js/mathics.js"></script>
-<script type="text/javascript" src="/media/js/graphics3d.js"></script>
-<script type="text/javascript" src="/media/js/doc.js"></script>"""
-            inject_javascript_code += self.read_static_file('media/js/prototype/prototype.js');
-            inject_javascript_code += self.read_static_file('media/js/three/Three.js');
-            inject_javascript_code += self.read_static_file('media/js/three/Detector.js');            
-            inject_javascript_code += self.read_static_file('media/js/utils.js');            
-            inject_javascript_code += self.read_static_file('media/js/mathics.js');            
-            inject_javascript_code += self.read_static_file('media/js/graphics3d.js');            
-
-
-            inject_javascript_code += '</script>'
-            
-            display_data = {
-                'data' : {'text/html' : inject_javascript_code},
-                'metadata' : {},
-            }
-            
-            self.send_response(self.iopub_socket, 'display_data', display_data)
-    """
     Handle jupyter connections.
     """
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
-        self.initialize_javascript_if_needed()
         
         if not silent:
             from mathics.core.parser import MultiLineFeeder
@@ -96,7 +49,7 @@ class MathicsNotebookKernel(Kernel):
             for result in results:
                 result_data = result.get_data()
                 
-                result_html = self.clear_output(result_data['result'])
+                result_html = result_data['result']
                 
                 display_data = {
                     'data' : {'text/html' : result_html},
@@ -110,6 +63,23 @@ class MathicsNotebookKernel(Kernel):
             'execution_count': self.execution_count,
             'payload': [],
             'user_expressions': {},
+        }
+
+    def do_complete(self, code, cursor_pos):
+        definitions = Definitions(add_builtin=True)
+
+        matches_raw = definitions.get_matching_names(code + '*')
+        matches     = []
+
+        for match in matches_raw:
+            matches.append(match.replace('System`', ''))
+
+        return {
+            'matches' : matches,
+            'cursor_start' : cursor_pos - len(code),
+            'cursor_end'   : cursor_pos,
+            'metadata'    : {},
+            'status' : 'ok',
         }
 
 if __name__ == '__main__':
